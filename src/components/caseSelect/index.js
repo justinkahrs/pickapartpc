9169b028cee3d4e9b77be027d2d1672935fb8837
Justin Kahrs <justin@justinkahrs.com>
2017-09-15 07:35:48 -0400

    added svg icons, confirmation button (still needs work)

--- src/components/caseSelect/index.js
+++ src/components/caseSelect/index.js
@@ -1,11 +1,16 @@
 // @flow
 import * as React from 'react';
-import { Collapse, Container, Label, Row } from 'reactstrap';
+import { Button, Collapse, Container, Label, Row } from 'reactstrap';
+import Check from 'react-icons/lib/md/check-circle';
 import Case from './Case';
 
-export default class CaseSelect extends React.Component<{}, { selectedCase: '' }> {
+export default class CaseSelect extends React.Component<
+  {},
+  { selectedCase: string, confirmed: boolean },
+> {
   state = {
     selectedCase: '',
+    confirmed: false,
   };
 
   styles = {
@@ -18,33 +23,55 @@ export default class CaseSelect extends React.Component<{}, { selectedCase: '' }
     label: {},
   };
 
-  select = (e: string) => {
+  selectCase = (e: string) => {
     this.setState({
-      selectedCase: this.state.selectedCase === e ? undefined : e,
+      selectedCase: this.state.selectedCase === e ? '' : e,
     });
+  };
+
+  confirmSelection = () => {
+    if (this.state.selectedCase !== '') {
+      this.setState({
+        confirmed: !this.state.confirmed,
+      });
+    }
   };
 
   render() {
     const { leftAlign, noBottomMargin } = this.styles;
-    const { selectedCase } = this.state;
+    const { confirmed, selectedCase } = this.state;
     return (
-      <Container style={leftAlign}>
+      <Container style={leftAlign} onClick={this.confirmSelection}>
         <Label style={noBottomMargin}>Case Size</Label>
         <hr />
-        <Row style={{ marginBottom: '3em' }}>
-          <Case height="5em" name="HTPC" slits={1} onClick={() => this.select('small')} />
-          <Case height="10em" name="Mid Tower" slits={3} onClick={() => this.select('medium')} />
-          <Case height="14em" name="Full Tower" slits={5} onClick={() => this.select('large')} />
-          <Collapse isOpen={selectedCase}>
-            <Label>
-              <strong>{selectedCase}</strong>
-            </Label>
-            <br />
-            {selectedCase !== '' ? (
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel libero varius, fermentum dolor vitae, semper elit. Praesent vel metus sodales sapien aliquam dignissim eget in metus. Mauris facilisis dui a est imperdiet ornare. Integer sem mauris, porttitor quis risus quis, consectetur commodo ex. Donec aliquam rhoncus metus non sollicitudin. Fusce tincidunt imperdiet ipsum, id rutrum nulla cursus eget. Nulla vulputate tempor eros non porttitor. Donec hendrerit ut mauris nec iaculis. Nam in leo tristique, lacinia tortor vel, suscipit ex. Donec in metus nisi. Sed vestibulum nunc nisi, in molestie diam sagittis vel. Ut dapibus nibh dapibus, lacinia elit a, pulvinar tortor.'
-            ) : null}
-          </Collapse>
-        </Row>
+        <Collapse isOpen={!confirmed}>
+          <Row style={{ marginBottom: '3em' }}>
+            <Case height="5em" name="HTPC" slits={1} onClick={() => this.selectCase('small')} />
+            <Case
+              height="10em"
+              name="Mid Tower"
+              slits={3}
+              onClick={() => this.selectCase('medium')}
+            />
+            <Case
+              height="14em"
+              name="Full Tower"
+              slits={5}
+              onClick={() => this.selectCase('large')}
+            />
+            <Collapse isOpen={selectedCase}>
+              <Label>
+                <strong>{selectedCase}</strong>
+              </Label>
+              <br />
+              {selectedCase !== '' ? (
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel libero varius, fermentum dolor vitae, semper elit. Praesent vel metus sodales sapien aliquam dignissim eget in metus. Mauris facilisis dui a est imperdiet ornare. Integer sem mauris, porttitor quis risus quis, consectetur commodo ex. Donec aliquam rhoncus metus non sollicitudin. Fusce tincidunt imperdiet ipsum, id rutrum nulla cursus eget. Nulla vulputate tempor eros non porttitor. Donec hendrerit ut mauris nec iaculis. Nam in leo tristique, lacinia tortor vel, suscipit ex. Donec in metus nisi. Sed vestibulum nunc nisi, in molestie diam sagittis vel. Ut dapibus nibh dapibus, lacinia elit a, pulvinar tortor.\n'
+              ) : null}
+              <Button onClick={this.confirmSelection}> Confirm Selection </Button>
+              <Check style={{ color: 'green' }} />
+            </Collapse>
+          </Row>
+        </Collapse>
       </Container>
     );
   }
--- src/pages/Build.js
+++ src/pages/Build.js
@@ -2,15 +2,11 @@
 import * as React from 'react';
 import { Container } from 'reactstrap';
 import CaseSelect from '../components/caseSelect';
-import CpuSelect from '../components/cpuSelect';
-import RamSelect from '../components/ramSelect';
 
 const Build = () => (
   <Container>
     <h1>Build</h1>
     <CaseSelect />
-    <CpuSelect />
-    <RamSelect />
   </Container>
 );
 export default Build;
